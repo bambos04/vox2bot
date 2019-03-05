541fc6483deb1da390a028f93d937836754710fd
bambos04 <48250476+bambos04@users.noreply.github.com>
2019-03-05 21:17:21 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -24,27 +24,27 @@ elseif( stristr ( $text, "/social" ) == true )
 {
 	$response = "Twitter: https://twitter.com/vox2box  \r\nInstagram: https://www.instagram.com/vox2box/ \r\nFacebook: https://www.facebook.com/vox2box/";
 }
-elseif( stristr ( $text, "/redazione" ) == true ) )
+elseif( stristr ( $text, "/redazione" ) == true ) 
 {
 	$response = "La gentile redazione è composta da 7 membri, in ordine di importanza: Marco Maioli, cinque a pari merito ovvero: Giulio Di Cienzo, Simone Donati, Francesco Lisanti, Francesco Mariani, Daniele Mazzanti e quell'altro, come si chiama, il sedicente direttore, Beppe Ruggiero";
 }
-elseif( stristr ( $text, "/sito" ) == true ) )
+elseif( stristr ( $text, "/sito" ) == true ) 
 {
 	$response = "HAHAHAHAHAHAHAHAHAHAHA, \r\nno va beh, tieni: https://www.vox2box.com";
 }
-elseif( stristr ( $text, "/salta" ) == true ) )
+elseif( stristr ( $text, "/salta" ) == true ) 
 {
 	$response = "$firstname salta con noi";
 }
-elseif( stristr ( $text, "/handanovic" ) == true ) )
+elseif( stristr ( $text, "/handanovic" ) == true )
 {
 	$response = "https://www.disabili.com/legge-e-fisco/speciali-legge-a-fisco/legge-104-disabili";
 }
-elseif( stristr ( $text, "/gruppi" ) == true ) )
+elseif( stristr ( $text, "/gruppi" ) == true )
 {
 	$response = "https://t.me/vox2box/490889";
 }
-elseif( stristr ( $text, "/puntata" ) == true ) )
+elseif( stristr ( $text, "/puntata" ) == true )
 {
 	$response = "https://www.spreaker.com/show/vox-2-box";
 }
