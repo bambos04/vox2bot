9534f6fb24183cd5b99f2718be73e96af0c299b0
bambos04 <48250476+bambos04@users.noreply.github.com>
2019-03-05 17:33:37 +0100

    Update execute.php

--- execute.php
+++ execute.php
@@ -17,35 +17,35 @@ $text = trim($text);
 $text = strtolower($text);
 header("Content-Type: application/json");
 $response = '';
-if(strpos($text, "/start") === 0 || $text=="/ciao" || $text=="/ciao@V0X2BOT")
+if(strpos($text, "/start") === 0 || $text=="/ciao" or $text=="/ciao@V0X2BOT")
 {
 	$response = "Ciao $firstname!";
 }
-elseif($text=="/social" || $text=="/social@V0X2BOT")
+elseif($text=="/social" or $text=="/social@V0X2BOT")
 {
 	$response = "Twitter: https://twitter.com/vox2box  \r\nInstagram: https://www.instagram.com/vox2box/ \r\nFacebook: https://www.facebook.com/vox2box/";
 }
-elseif($text=="/redazione" || $text=="/redazione@V0X2BOT")
+elseif($text=="/redazione" or $text=="/redazione@V0X2BOT")
 {
 	$response = "La gentile redazione è composta da 7 membri, in ordine di importanza: Marco Maioli, cinque a pari merito ovvero: Giulio Di Cienzo, Simone Donati, Francesco Lisanti, Francesco Mariani, Daniele Mazzanti e quell'altro, come si chiama, il sedicente direttore, Beppe Ruggiero";
 }
-elseif($text=="/sito" || $text=="/sito@V0X2BOT")
+elseif($text=="/sito" or $text=="/sito@V0X2BOT")
 {
 	$response = "HAHAHAHAHAHAHAHAHAHAHA, no va beh, tieni: https://www.vox2box.com";
 }
-elseif($text=="/salta" || $text=="/salta@V0X2BOT")
+elseif($text=="/salta" or $text=="/salta@V0X2BOT")
 {
 	$response = "$firstname salta con noi";
 }
-elseif($text=="/handanovic" || $text=="/handanovic@V0X2BOT")
+elseif($text=="/handanovic" or $text=="/handanovic@V0X2BOT")
 {
 	$response = "https://www.disabili.com/legge-e-fisco/speciali-legge-a-fisco/legge-104-disabili";
 }
-elseif($text=="/gruppi" || $text=="/gruppi@V0X2BOT")
+elseif($text=="/gruppi" or $text=="/gruppi@V0X2BOT")
 {
 	$response = "https://t.me/vox2box/481340";
 }
-elseif($text=="/puntata" || $text=="/puntata@V0X2BOT")
+elseif($text=="/puntata" or $text=="/puntata@V0X2BOT")
 {
 	$response = "https://www.spreaker.com/show/vox-2-box";
 }
